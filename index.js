5dc24856abace94e9e960b41111948ee05898319
Pallav Gupta <palforwork2@gmail.com>
2024-07-18 23:32:00 +0530

    Setup electron app

---
+++ index.js
@@ -0,0 +1,38 @@
+// main.js
+
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  // Load the index.html file.
+  mainWindow.loadFile('index.html');
+
+  // Open the DevTools (optional).
+  // mainWindow.webContents.openDevTools();
+}
+
+// This method will be called when Electron has finished initialization.
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed, except on macOS.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On macOS, recreate a window when the dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
